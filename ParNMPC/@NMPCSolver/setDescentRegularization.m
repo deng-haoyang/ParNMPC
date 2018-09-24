1f169f2b364a712cdb033060bee82ca0f4b707ce
Deng Haoyang <deng.haoyang.1993@gmail.com>
2018-09-24 10:42:07 +0900

    Add regularization

--- ParNMPC/@NMPCSolver/setDescentRegularization.m
+++ ParNMPC/@NMPCSolver/setDescentRegularization.m
@@ -2,7 +2,9 @@ function setDescentRegularization(solver,value)
 % descent regularization parameter
 % nonsingular regularization parameter
 if value > 0
+    global descentRegularization
     solver.descentRegularization = value;
+    descentRegularization = value;
 else
     error('Descent regularization parameter must be nonnegtive!');
 end
--- ParNMPC/@NMPCSolver/setNonsingularRegularization.m
+++ ParNMPC/@NMPCSolver/setNonsingularRegularization.m
@@ -1,7 +1,9 @@
 function setNonsingularRegularization(solver,value)
 % nonsingular regularization parameter
 if value > 0
+    global nonsingularRegularization
     solver.nonsingularRegularization = value;
+    nonsingularRegularization = value;
 else
     warning('Nonsingular regularization parameter must be nonnegtive!');
 end
